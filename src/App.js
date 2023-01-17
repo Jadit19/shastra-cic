c87c41e2e3e31afeb1add5e0cbe380cd138cde57
Jadit19 <jadit19@gmail.com>
2023-01-17 04:17:41 +0000

    major UI updation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React, { useState } from 'react';
-import dayjs from 'dayjs';
+
 import { TextField, Button } from '@mui/material';
-import { MobileDateTimePicker, LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+
 import { Stack } from '@mui/system';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { MobileDateTimePicker, LocalizationProvider } from '@mui/x-date-pickers';
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+import { ComposableMap, Geographies, Geography, Graticule, Marker } from 'react-simple-maps';
+
+import dayjs from 'dayjs';
 
 import { calculate } from './calculator';
 
+import './index.css';
+
+const geoURL = "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json";
+
 const App = () => {
+  const inputStyle = {
+    marginBottom: '20px',
+    width: '100%'
+  };
+  
+  const [coordinates, setCoordinates] = useState(null);
   const [params, setParams] = useState({
     sma: 0,
     inclination: 0,
@@ -17,22 +40,25 @@ const App = () => {
     periapsis: 0,
     time: dayjs(Date().toString())
   });
-  const [llh, setLLH] = useState(null);
 
-  const inputStyle = {
-    marginBottom: '20px',
-    width: '100%'
-  };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+  const handleChange = (event) => {
+    const { name, value } = event.target;
     setParams({
       ...params,
       [name]: value
     });
   };
 
+  const handleTimeChange = (event) => {
+    const newTime = new Date(event.$d);
+    setParams({
+      ...params,
+      time: dayjs(newTime.toString())
+    });
+  };
+
   const handleReset = () => {
+    setCoordinates(null);
     setParams({
       sma: 0,
       inclination: 0,
@@ -42,78 +68,98 @@ const App = () => {
       periapsis: 0,
       time: dayjs(Date().toString())
     });
-    setLLH(null);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const answer = calculate(params);
-    setLLH(answer);
+    const newCoordinates = calculate(params);
+    setCoordinates(newCoordinates);
   };
 
   return (
-    <div className='app'>
+    <>
       <h1>Shastra</h1>
-      <form onSubmit={handleSubmit} onReset={handleReset}>
-        <div className='form__field'><TextField style={inputStyle} type='number' variant='outlined' label='Semi-Major Axis (in m)' name='sma' required value={params.sma} onChange={handleChange} /></div>
-        <div className='form__field'><TextField style={inputStyle} type='number' variant='outlined' label='Inclination (in degrees)' name='inclination' required value={params.inclination} onChange={handleChange} /></div>
-        <div className='form__field'><TextField style={inputStyle} type='number' variant='outlined' label='RAAN (in degrees)' name='raan' required value={params.raan} onChange={handleChange} /></div>
-        <div className='form__field'><TextField style={inputStyle} type='number' variant='outlined' label='Eccentricity' name='eccentricity' required value={params.eccentricity} onChange={handleChange} /></div>
-        {
-          params.eccentricity === 0
-            ? <div></div>
-            : <div className='form__field'><TextField style={inputStyle} type='number' variant='outlined' label='Argument of Periapsis' name='periapsis' required value={params.periapsis} onChange={handleChange} /></div>
-        }
-        <div className='form__field'><TextField style={inputStyle} type='number' variant='outlined' label='True Anomaly (in degrees)' name='anomaly' required value={params.anomaly} onChange={handleChange} /></div>
-        <div className='form__field'>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <Stack spacing={3}>
-              <MobileDateTimePicker
-                value={params.time}
-                onChange={handleChange}
-                label='UTC'
-                onError={console.log}
-                minDate={dayjs('2018-01-01T00:00')}
-                inputFormat="YYYY/MM/DD hh:mm a"
-                mask="____/__/__ __:__ _M"
-                renderInput={(p) => <TextField { ...p } />}
-              />
-            </Stack>
-          </LocalizationProvider>
+      <div className='app'>
+
+        <div className='container'>
+          <form onReset={handleReset} onSubmit={handleSubmit}>
+            <div className='form__field'><TextField style={inputStyle} type='number' variant='outlined' label='Semi-Major Axis (in m)' name='sma' required value={params.sma} onChange={handleChange} /></div>
+            <div className='form__field'><TextField style={inputStyle} type='number' variant='outlined' label='Inclination (in degrees)' name='inclination' required value={params.inclination} onChange={handleChange} /></div>
+            <div className='form__field'><TextField style={inputStyle} type='number' variant='outlined' label='RAAN (in degrees)' name='raan' required value={params.raan} onChange={handleChange} /></div>
+            <div className='form__field'><TextField style={inputStyle} type='number' variant='outlined' label='Eccentricity' name='eccentricity' required value={params.eccentricity} onChange={handleChange} /></div>
+            {
+              params.eccentricity === 0
+                ? null
+                : <div className='form__field'><TextField style={inputStyle} type='number' variant='outlined' label='Argument of Periapsis (in degrees)' name='periapsis' required value={params.periapsis} onChange={handleChange} /></div>
+            }
+            <div className='form__field'><TextField style={inputStyle} type='number' variant='outlined' label='True Anomaly (in degrees)' name='anomaly' required value={params.anomaly} onChange={handleChange} /></div>
+            <div className='form__field'>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <Stack spacing={3}>
+                  <MobileDateTimePicker
+                    value={params.time}
+                    onChange={handleTimeChange}
+                    label='UTC'
+                    onError={console.log}
+                    inputFormat="YYYY/MM/DD hh:mm a"
+                    mask="____/__/__ __:__ _M"
+                    renderInput={(p) => <TextField required { ...p } />}
+                  />
+                </Stack>
+              </LocalizationProvider>
+            </div>
+            <div className='btn__container'>
+              <Button variant='outlined' type='reset'>Reset</Button>
+              <Button variant='contained' type='submit'>Submit</Button>
+            </div>
+          </form>
         </div>
-        <div className='btn__container'>
-          <Button variant='outlined' type='reset'>Reset</Button>
-          <Button variant='contained' type='submit'>Submit</Button>
+
+        <div className='container'>
+          <div className='table'>
+            <TableContainer componenet={Paper}>
+              <Table aria-label='simple table'>
+                <TableHead>
+                  <TableRow>
+                    <TableCell style={{ backgroundColor: 'rgba(0,0,0,0.1)', fontWeight: 'bold' }}>Location in GCS</TableCell>
+                    <TableCell style={{ backgroundColor: 'rgba(0,0,0,0.1)', fontWeight: 'bold' }}>Value</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  <TableRow>
+                    <TableCell>Longitude</TableCell>
+                    <TableCell>{ coordinates === null ? 'NA' : coordinates.longitude }</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>Latitude</TableCell>
+                    <TableCell>{ coordinates === null ? 'NA' : coordinates.latitude }</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
+
+          <div className='map'>
+            <ComposableMap projectionConfig={{ scale: 140 }}>
+              <Graticule stroke="rgba(0,0,0,0.1)" />
+              <Geographies geography={geoURL}>
+              {({ geographies }) =>
+                geographies.map((geo) => (
+                  <Geography key={geo.rsmKey} geography={geo} fill='rgba(0,0,0,0.6)' />
+                ))
+              }
+              </Geographies>
+              {
+                (coordinates===null) ? null :
+                  <Marker coordinates={[coordinates.longitude, coordinates.latitude]}>
+                    <circle r={7} fill='red' />
+                  </Marker>
+              }
+            </ComposableMap>
+          </div>
         </div>
-      </form>
-
-      <br />
-      <hr style={{ width: '100%', color: 'rgba(0,0,0,0.6)' }} />
-
-      {
-        llh === null
-          ? <div></div>
-          :
-          <table>
-            <tbody>
-              <tr>
-                <th>Location in GCS</th>
-                <th>Value</th>
-              </tr>
-              <tr>
-                <td>Longitude</td>
-                <td>{ llh.longitude }</td>
-              </tr>
-              <tr>
-                <td>Latitude</td>
-                <td>{ llh.latitude }</td>
-              </tr>
-            </tbody>
-          </table>
-      }
-
-      
-    </div>
+      </div>
+    </>
   );
 };
 
